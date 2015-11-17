b969305b4a1c669a4a87d11dd4419ee27478c678
Developer <klevtsovstanislav@gmial.com>
2015-11-17 03:56:19 +0300

    Create heapq.py

---
+++ heapq.py
@@ -0,0 +1,26 @@
+import heapq
+
+def _heappush_max(heap, item):
+    heap.append(item)
+    heapq._siftdown_max(heap, 0, len(heap)-1)
+
+def _heappop_max(heap):
+    lastelt = heap.pop() 
+    if heap:
+        returnitem = heap[0]
+        heap[0] = lastelt
+        heapq._siftup_max(heap, 0)
+        return returnitem
+    return lastelt
+
+h=[]
+n = int(input())
+i = 0
+heapq._heapify_max(h)
+while i<n:
+        s = str(input())
+        if s.split()[0] =='ExtractMax':
+            print(_heappop_max(h))
+        elif s.split()[0] =='Insert':
+            _heappush_max(h,int(s.split()[1]))
+        i = i + 1
