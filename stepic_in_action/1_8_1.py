b77abe390fb08a21bb68e99d6c27efd143e8d3f3
Developer <klevtsovstanislav@gmial.com>
2016-06-24 00:30:37 +0300

    Create 1_8_1.py

---
+++ stepic_in_action/1_8_1.py
@@ -0,0 +1,21 @@
+При каких значениях переменной x будет выведена фраза?
+
+if not x:
+    print('x like false value!')
+    
+    
+  http://pythontutor.com/visualize.html#mode=display
+  
+  [None, None]
+ []
+ {}
+ 0
+ float('inf')
+ float('nan')
+ True
+ set()
+ [None]
+ object()
+ ()
+ 0j
+ False
