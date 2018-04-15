f7c566ec8a717f792d607ddb6316cc330a1798f7
Developer <klevtsovstanislav@gmial.com>
2018-04-16 02:21:07 +0300

    Update tbl_round.sql

--- Arkadium/tbl_round.sql
+++ Arkadium/tbl_round.sql
@@ -1,7 +1,7 @@
 USE [Arkadium]
 GO
 
-/****** Object:  Table [dbo].[tbl_round]    Script Date: 15.04.2018 22:13:20 ******/
+/****** Object:  Table [dbo].[tbl_round]    Script Date: 16.04.2018 2:20:51 ******/
 SET ANSI_NULLS ON
 GO
 
@@ -10,10 +10,12 @@ GO
 
 CREATE TABLE [dbo].[tbl_round](
 	[round_id] [int] NOT NULL,
-	[round_name] [varchar](16) NOT NULL,
+	[round_name] [nvarchar](16) NOT NULL,
  CONSTRAINT [PK_tbl_round] PRIMARY KEY CLUSTERED 
 (
 	[round_id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
+
+
