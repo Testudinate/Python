8485f7765a373765a8b20a9ca2b1639b36a23713
Developer <klevtsovstanislav@gmial.com>
2018-04-15 22:15:44 +0300

    Create tbl_main.sql

---
+++ Arkadium/tbl_main.sql
@@ -0,0 +1,31 @@
+USE [Arkadium]
+GO
+
+/****** Object:  Table [dbo].[tbl_main]    Script Date: 15.04.2018 22:15:09 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[tbl_main](
+	[boxer_name] [varchar](16) NOT NULL,
+	[result] [varchar](5) NULL,
+	[country_id] [int] NOT NULL,
+	[round_id] [int] NULL
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[tbl_main]  WITH CHECK ADD  CONSTRAINT [FK_tbl_main_tbl_country] FOREIGN KEY([country_id])
+REFERENCES [dbo].[tbl_country] ([country_id])
+GO
+
+ALTER TABLE [dbo].[tbl_main] CHECK CONSTRAINT [FK_tbl_main_tbl_country]
+GO
+
+ALTER TABLE [dbo].[tbl_main]  WITH CHECK ADD  CONSTRAINT [FK_tbl_main_tbl_round] FOREIGN KEY([round_id])
+REFERENCES [dbo].[tbl_round] ([round_id])
+GO
+
+ALTER TABLE [dbo].[tbl_main] CHECK CONSTRAINT [FK_tbl_main_tbl_round]
+GO
